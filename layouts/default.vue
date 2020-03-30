c651b86b885c90c5ebffc6f31d35df5b399b65e1
Stewart Noll <stewartnoll@gmail.com>
2020-03-29 22:58:41 -0500

    remove unused code

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,13 +7,3 @@
     </v-content>
   </v-app>
 </template>
-
-<script>
-export default {
-  data () {
-    return {
-      e6: 1
-    }
-  }
-}
-</script>
--- pages/index.vue
+++ pages/index.vue
@@ -37,14 +37,7 @@
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
-  },
   data() {
     return {
       e6: 1,
